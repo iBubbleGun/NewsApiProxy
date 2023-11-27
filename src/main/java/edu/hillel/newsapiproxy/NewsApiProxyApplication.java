75dee17cd4963d9962b718e4ea4e9c775dc2d357
Tymur Kosiak <us1gav@gmail.com>
2023-11-27 23:09:53 +0200

    NewsApiProxy server creates.

---
+++ src/main/java/edu/hillel/newsapiproxy/NewsApiProxyApplication.java
@@ -0,0 +1,13 @@
+package edu.hillel.newsapiproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsApiProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsApiProxyApplication.class, args);
+    }
+
+}
